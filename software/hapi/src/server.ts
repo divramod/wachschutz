71bfc25898c21724612015f68b399c03cd0867f8
divramod <divramod@gmail.de>
2016-06-01 20:13:23 +0200

    update

--- software/hapi/src/server.ts
+++ software/hapi/src/server.ts
@@ -5,6 +5,27 @@ import * as Hapi from "hapi";
 import Routes from "./routes";
 const fs = require('fs');
 const path = require('path');
+var Gpio = require('onoff').Gpio;
+var led = new Gpio(14, 'out');
+var button = new Gpio(4, 'in', 'both');
+
+button.watch(function (err, value) {
+  if (err) {
+    throw err;
+  }
+
+  if (value) {
+    console.log("button pressed");
+  } else {
+    console.log("button released");
+  }
+  led.writeSync(value);
+});
+
+process.on('SIGINT', function () {
+  led.unexport();
+  button.unexport();
+});
 
 var port = process.env.port || Configurations.Server.port;
 var server = new Hapi.Server();
