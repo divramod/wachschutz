d574980e099b412a45245e46d3d1e7c2be288525
divramod <divramod@gmail.de>
2016-06-01 18:15:44 +0200

    update

--- software/server-api-hardware/server.js
+++ software/server-api-hardware/server.js
@@ -1,8 +1,8 @@
 var Gpio = require('onoff').Gpio;
-//console.log("hello world");
+console.log("hello world");
 var led = new Gpio(14, 'out');
-//var button = new Gpio(4, 'in', 'both');
+var button = new Gpio(4, 'in', 'both');
 
-//button.watch(function(err, value) {
-  //led.writeSync(value);
-//});
+button.watch(function(err, value) {
+  led.writeSync(value);
+});
